af4686c1ef2ba0fbc5e551708e8299064775fae7
Colm Andrews <73839417+andrewscolm@users.noreply.github.com>
2026-08-17 14:43:55 +0100

    add functions for write up

--- analysis/functions/functions.r
+++ analysis/functions/functions.r
@@ -1,2 +1,39 @@
 label_number_n <- label_number(1, 1, big.mark = ",")
 label_perc_n <- label_percent(accuracy = .0001)
+
+
+collapse_and <- function(x) {
+  x <- unlist(x, use.names = FALSE)
+  n <- length(x)
+
+  if (n == 0) {
+    return("")
+  }
+  if (n == 1) {
+    return(x)
+  }
+  if (n == 2) {
+    return(paste(x, collapse = " and "))
+  }
+
+  paste0(
+    paste(x[seq_len(n - 1)], collapse = ", "),
+    ", and ",
+    x[n]
+  )
+}
+
+
+no_or_num <- function(x) {
+  n <- nrow(x)
+
+  if (n == 0) {
+    return("No ICB")
+  }
+  if (n == 1) {
+    return("1 ICB")
+  }
+  if (n > 1) {
+    return(glue("{n} ICBs"))
+  }
+}
